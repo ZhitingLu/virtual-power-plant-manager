c40ff84b734b5329448933355571fd0be2124795
Zhiting Lu <zhitinglu@outlook.com>
2025-07-21 21:22:42 +0200

    Refactor app.module.ts to include ConfigModule for global environment variable management and update tsconfig.json to ensure test files are included for type checking

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+// Importing the ConfigModule to manage environment variables
+// and make them available globally in the application
+
+// Importing feature modules
 import { PowerPlantModule } from './power-plant/power-plant.module';
 import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
+      ConfigModule.forRoot({
+      isGlobal: true, // ✅ Makes ConfigService available everywhere
+      envFilePath: '.env', // Can also use an array for multi-env support
+    }),
     PowerPlantModule, // Our feature module
     EventsModule, // Event handling module
   ],
